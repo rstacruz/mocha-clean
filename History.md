8ecd801ec8a51fa1ba34da6b3c23e044ddd19791
Rico Sta. Cruz <hi@ricostacruz.com>
2014-09-25 12:32:59 +0800

    Clean up.

--- History.md
+++ History.md
@@ -1,3 +1,7 @@
+## v0.1.1 - September 25, 2014
+
+* Clean up internal node module.js errors.
+
 ## v0.1.0 - September 25, 2014
 
 * Big refactor to support async and stuff.
--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var Mocha = require('mocha');
 var Runner = Mocha.Runner;
 
 /*
- * monkey-patch Runner#fail to modify `err`
+ * monkey-patch Runner#fail to modify `err`.
  */
 
 var old = Runner.prototype.fail;
 Runner.prototype.fail = function (test, err) {
   err = __mocha_internal__cleanError(err);
-  old.call(this, test, err);
+  return old.call(this, test, err);
 };
 
 /*
- * cleans an error
+ * cleans an error.
  */
 
 function __mocha_internal__cleanError (e) {
@@ -22,23 +22,14 @@ function __mocha_internal__cleanError (e) {
   var stack = e.stack.split('\n');
 
   stack = stack.reduce(function (list, line) {
-    // supress node modules
-    if (~line.indexOf('node_modules') && !process.env.SHOW_NODE_MODULES)
+    // Strip out certain lines.
+    if (isNodeModule(line) ||
+      isMochaInternal(line) ||
+      isNodeInternal(line) ||
+      isMochaCleanInternal(line))
       return list;
 
-    // supress mocha internals
-    if (~line.indexOf('node_modules' + slash + 'mocha'))
-      return list;
-
-    // node internals
-    if ((~line.indexOf('(timers.js:')) ||
-        (~line.indexOf('(module.js:')))
-      return list;
-
-    // supress ourselves
-    if (~line.indexOf('__mocha_internal__'))
-      return list;
-
+    // Clean up cwd.
     line = line.replace(cwd, '');
     list.push(line);
     return list;
@@ -49,7 +40,50 @@ function __mocha_internal__cleanError (e) {
 }
 
 /*
- * export for tests
+ * detect if a line is from a 3rd-party module.
+ */
+
+function isNodeModule (line) {
+  return (~line.indexOf('node_modules')) &&
+    !process.env.SHOW_NODE_MODULES;
+}
+
+/*
+ * detect stuff from mocha itself.
+ * usually not needed, but if SHOW_NODE_MODULES is on, you probably wanna
+ * supress this.
+ */
+
+function isMochaInternal (line) {
+  return (~line.indexOf('node_modules' + slash + 'mocha'));
+}
+
+/*
+ * detect stuff from this library.
+ */
+
+function isMochaCleanInternal (line) {
+  return (~line.indexOf('__mocha_internal__'));
+}
+
+/*
+ * detect internal node errors. Examples:
+ *
+ *   at Module._compile (module.js:439:25)
+ *   at Object.Module._extensions..js (module.js:474:10)
+ *   at Module.load (module.js:356:32)
+ *   at Function.Module._load (module.js:312:12)
+ *   at Module.require (module.js:364:17)
+ *   at require (module.js:380:17)
+ */
+
+function isNodeInternal (line) {
+  return (~line.indexOf('(timers.js:')) ||
+    (~line.indexOf('(module.js:'));
+}
+
+/*
+ * export for tests.
  */
 
 exports.cleanError = __mocha_internal__cleanError;
